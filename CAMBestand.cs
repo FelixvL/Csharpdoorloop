b5c7f6ef8f96d084498f7a9c9203c4a86e828827
felixvl <digitaleservice@gmail.com>
2025-03-25 13:09:26 +0100

    go

--- CAMBestand.cs
+++ CAMBestand.cs
@@ -9,6 +9,14 @@ namespace abc
     internal class CAMBestand
     {
         internal string instructietext;
+
+        internal string bestandsnaam;
+
+        public CAMBestand(string hout){
+            bestandsnaam = hout;
+            Console.WriteLine("er is een cambestand aangemaakt");
+        }
+
         internal void toonInhoud() {
             Console.WriteLine(instructietext);
         }
--- Program.cs
+++ Program.cs
@@ -6,24 +6,30 @@ namespace abc{
      
 
             //CAMBEstand klasse  field   instructiebestand string    toon instructie
-            CAMBestand cmbestand = new CAMBestand();
+            CAMBestand cmbestand = new CAMBestand("houtzagen.txt");
             cmbestand.instructietext = "ffzagen;hakken4cm;";
             cmbestand.toonInhoud();
-            Auto a = new Auto("Audi");
-            new Auto("Audi");
-            Auto b = new Auto("Mercedes");
-            Console.WriteLine(a.merk);
-            Console.WriteLine(b.merk);
-            b.setSnelheid(-48);
-            b.toonSnelheid();
-            b.AantalPersonen = -3;
-            Console.WriteLine(b.AantalPersonen);
+            Console.WriteLine(cmbestand.bestandsnaam);
+            //Auto a = new Auto("Audi");
+            //new Auto("Audi");
+            //Auto b = new Auto("Mercedes");
+            //Console.WriteLine(a.merk);
+            //Console.WriteLine(b.merk);
+            ////b.snelheid = -48; 
+            //b.setSnelheid(-48);
+            //b.toonSnelheid();
+            //b.AantalPersonen = -3;
+            //Console.WriteLine(b.AantalPersonen);
+            //b.AantalPersonen = 5;
+            //b.AantalCC = 4;
         }
     }
     class Auto {
         internal string merk;
         private int snelheid;
         private int aantalPersonen;
+
+        public int AantalCC { get; set; }
 
         public int AantalPersonen {
             get { return aantalPersonen; }
@@ -39,7 +45,7 @@ namespace abc{
         }
 
         public void setSnelheid(int speed) {
-            if (speed < 0)
+            if (speed < 0 || speed > 220)
             {
                 Console.WriteLine("doe niet zo raar");
             }
