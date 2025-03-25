a15ad7570b4f2a0906e59d26b27b46b69d2a9281
Ronald-somebody <ronald.leerink@nl.thalesgroup.com>
2025-03-25 11:59:26 +0100

    1

---
+++ CAMBestand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abc
+{
+    internal class CAMBestand
+    {
+        internal string instructietext;
+        internal void toonInhoud() {
+            Console.WriteLine(instructietext);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace abc{
+    class Program {
+        public static void Main(string[] args) {
+     
+
+            //CAMBEstand klasse  field   instructiebestand string    toon instructie
+            CAMBestand cmbestand = new CAMBestand();
+            cmbestand.instructietext = "ffzagen;hakken4cm;";
+            cmbestand.toonInhoud();
+
+        }
+    }
+
+}
